25008b6c9aca2da3f3dc15eba3e07f997e5f4325
SpaceSC <ceciliasandor.22@gmail.com>
2021-02-10 10:34:49 +0100

    Add test

--- test1/src/App.test.js
+++ test1/src/App.test.js
@@ -1,5 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { unmountComponentAtNode } from "react-dom";
+
+let container = null;
+beforeEach(() => {
+  // setup a DOM element as a render target
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  // cleanup on exiting
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
 
 test('renders learn react link', () => {
   render(<App />);
@@ -8,6 +23,7 @@ test('renders learn react link', () => {
 });
 
 test('renders a element', () => {
+  
   render(<App />);
   expect(screen.getByText('Learn React')).toBeInTheDocument();
 });
